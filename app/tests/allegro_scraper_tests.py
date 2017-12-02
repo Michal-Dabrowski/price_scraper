f2134dc8011cd31e81504fe7f34b6ddfec345454
Michal Dabrowski <canthemedia@gmail.com>
2017-12-02 20:31:46 +0100

    Added more tests

--- app/tests/allegro_scraper_tests.py
+++ app/tests/allegro_scraper_tests.py
@@ -9,7 +9,7 @@ with open('allegro.txt', 'rb') as file:
     content = file.read()
 test_soup = BeautifulSoup(content, 'html.parser')
 scraper = AllegroScraper('')
-test_product = scraper.search_soup_for_json_object(test_soup)['regular'][0]
+
 
 class SearchSoupForJsonObjectTestCase(unittest.TestCase):
 
@@ -25,18 +25,74 @@ class SearchSoupForJsonObjectTestCase(unittest.TestCase):
 
     def test_if_json_has_promoted_items(self):
         scraper = AllegroScraper('')
-        assert 'promoted' in scraper.search_soup_for_json_object(test_soup)
+        self.assertTrue('promoted' in scraper.search_soup_for_json_object(test_soup))
 
     def test_if_json_has_regular_items(self):
         scraper = AllegroScraper('')
-        assert 'regular' in scraper.search_soup_for_json_object(test_soup)
+        self.assertTrue('regular' in scraper.search_soup_for_json_object(test_soup))
 
     def test_if_json_has_sponsored_items(self):
         scraper = AllegroScraper('')
-        assert 'sponsored' in scraper.search_soup_for_json_object(test_soup)
+        self.assertTrue('sponsored' in scraper.search_soup_for_json_object(test_soup))
 
 class ProductIsBuynowOptionTestCase(unittest.TestCase):
 
     def test_buynow(self):
-        scraper = AllegroScraper('')
-        assert  scraper.product_is_buynow_option(test_product)
+        test_product_buynow = {'sellingMode': {'sellingMode': 'buyNow'}}
+        self.assertTrue(scraper.product_is_buynow_option(test_product_buynow))
+
+    def test_auction(self):
+        test_product_auction = {'sellingMode': {'sellingMode': 'auction'}}
+        self.assertFalse(scraper.product_is_buynow_option(test_product_auction))
+
+class IsProductNewTestCase(unittest.TestCase):
+
+    def test_new(self):
+        product = {'parameters': [{'values': ['Nowy']}]}
+
+
+
+
+
+json = {'vendor': 'allegro',
+        'location': {'city': 'Kraków',
+                     'country': 'PL'
+                     },
+        'name': 'Reloop Beatmix 2 Mk2 kontroler DJ+słuchawki GRATIS',
+        'parameters': [{'highlight': False,
+                        'values': ['Nowy'],
+                        'name': 'Stan'
+                        }
+                       ],
+        'id': '7056549951',
+        'seller': {'superSeller': True,
+                   'id': '7292182'},
+        'url': 'http://allegro.pl/reloop-beatmix-2-mk2-kontroler-dj-sluchawki-gratis-i7056549951.html',
+        'coins': {'total': {'quantity': 0}
+                  },
+        'images': [{'url': 'https://7.allegroimg.com/original/019d2c/0eb579e14af6a0822f846631a2d7'}],
+        'categoryPath': [{'id': '122332'},
+                         {'id': '122370'},
+                         {'id': '122371'}
+                         ],
+        'quantity': {'value': 9, 'unitType': 'UNIT'},
+        'sellingMode': {'sellingMode': 'auction',
+                        'buyNow': {'cartAvailable': True,
+                                   'popularity': 1,
+                                   'price': {'currency': 'PLN',
+                                             'amount': '765.00'}
+                                   }
+                        },
+        'shipping': {'lowest': {'currency': 'PLN',
+                                'amount': '0.00'
+                                },
+                     'itemWithDelivery': {'currency': 'PLN',
+                                          'amount': '765.00'
+                                          },
+                     'freeDeliveryWeek': False,
+                     'freeDelivery': True,
+                     'freeReturn': True},
+        'promotion': {'bold': False,
+                      'highlight': False,
+                      'emphasized': False}
+        }
