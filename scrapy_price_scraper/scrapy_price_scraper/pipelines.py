ea516e26ce42e46f4388a10113cfdf4d4b079fd0
mdabrowski <mdabrowski@teonite.com>
2018-05-16 22:26:13 +0200

    added scrapy pipelines

--- scrapy_price_scraper/scrapy_price_scraper/pipelines.py
+++ scrapy_price_scraper/scrapy_price_scraper/pipelines.py
@@ -10,12 +10,9 @@ from app.models import detect_name_and_suggested_price, count_percentage_decreas
 
 class AllegroPipeline(object):
     def process_item(self, item, spider):
-        try:
-            name_and_price = detect_name_and_suggested_price(item['full_name'])
-            item['product_name'] = name_and_price['name']
-            item['suggested_price'] = name_and_price['suggested_price']
-            item['percentage_decrease'] = count_percentage_decrease(item['suggested_price'], item['price'])
-            item['price_too_low'] = item['price'] < item['suggested_price']
-        except TypeError:
-            pass
+        name_and_price = detect_name_and_suggested_price(item['full_name'])
+        item['product_name'] = name_and_price['name']
+        item['suggested_price'] = name_and_price['suggested_price']
+        item['percentage_decrease'] = count_percentage_decrease(item['suggested_price'], item['price'])
+        item['price_too_low'] = item['price'] < item['suggested_price']
         return item
--- scrapy_price_scraper/scrapy_price_scraper/spiders/allegro_scraper.py
+++ scrapy_price_scraper/scrapy_price_scraper/spiders/allegro_scraper.py
@@ -1,9 +1,9 @@
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../..'))
-
 import scrapy
 import json
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../..'))
 from config import BRAND_NAME
 
 
